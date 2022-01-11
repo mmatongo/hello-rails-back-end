783be977aeec3abba909664ab6d0a39bbb25a99f
Daniel M. Matongo <m_matongo@icloud.com>
2022-01-11 14:33:33 +0200

    fix linter errors

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module HellorailsBackend
+  # this comment is meant to silence rubocop errors
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
