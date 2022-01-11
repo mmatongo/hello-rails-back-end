43f422249c6513ad7b97bcd668e191dabce675e5
Daniel M. Matongo <m_matongo@icloud.com>
2022-01-11 14:55:39 +0200

    fixes

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,7 @@
+# creates the api json ouput and randomises it
+class GreetingsController < ApplicationController
+  def index
+    greeting = Greeting.order(Arel.sql('RANDOM()')).first
+    render json: { greeting: greeting.greeting }
+  end
+end
